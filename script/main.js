a8d7bfb3c0e360110af85cc5eb3ffde149f7e4ff
Martin Miloslav Suquillo Tigasi <116769944+31MartinS@users.noreply.github.com>
2025-02-01 19:15:28 -0500

    fix 4

--- script/main.js
+++ script/main.js
@@ -1,12 +1,5 @@
 // Animation Timeline
 const animationTimeline = () => {
-
-    // Reproducir la música al hacer clic en el botón
-  document.getElementById('playButton').addEventListener('click', () => {
-    const audio = document.getElementById('backgroundMusic');
-    audio.play();  // Inicia la reproducción de la música
-  });
-
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
