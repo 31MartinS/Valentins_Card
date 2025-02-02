889bc2d62b2c0cb90c9cf4b979b07aaa319a5269
Martin Miloslav Suquillo Tigasi <116769944+31MartinS@users.noreply.github.com>
2025-02-02 13:39:04 -0500

    v1.2

--- script/main.js
+++ script/main.js
@@ -12,9 +12,16 @@ const animationTimeline = () => {
     .join("</span><span>")}</span>`;
 
   // Código para reproducir la música de fondo
-  const audio = new Audio('ipma.mp3');
+  const audio = new Audio('ipma.ogg');
+  audio.preload = 'auto'; // Pre-cargar el audio
   audio.loop = true; // Repetir la música
-  audio.play(); // Comenzar la música
+
+  // Asegurarse de que el audio se ha cargado antes de reproducirlo
+  audio.play().then(() => {
+    console.log("Música iniciada");
+  }).catch((error) => {
+    console.log("Error al reproducir música: ", error);
+  });
 
   const ideaTextTrans = {
     opacity: 0,
